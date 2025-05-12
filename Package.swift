8390462d3b7b1e3a42ca003d6919bc319d8dab32
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2025-05-12 21:33:28 +0200

    chore: i have no fucking idea

--- Package.swift
+++ Package.swift
@@ -4,33 +4,33 @@
 import PackageDescription
 
 let package = Package(
-    name: "DiscordSocialKit",
-    platforms: [
-        .iOS(.v17),
-        .macOS(.v14),
-    ],
-    products: [
-        .library(
-            name: "DiscordSocialKit",
-            targets: ["DiscordSocialKit"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/rryam/MusadoraKit", from: "6.1.0")
-    ],
-    targets: [
-        .target(
-            name: "DiscordSocialKit",
-            dependencies: [
-                "MusadoraKit",
-                "discord_partner_sdk"
-            ]),
-        .binaryTarget(
-            name: "discord_partner_sdk",
-            path: "Frameworks/discord_partner_sdk.xcframework"
-        ),
-        .testTarget(
-            name: "DiscordSocialKitTests",
-            dependencies: ["DiscordSocialKit"]
-        ),
-    ]
+	name: "DiscordSocialKit",
+	platforms: [
+		.iOS(.v17),
+		.macOS(.v14),
+	],
+	products: [
+		.library(
+			name: "DiscordSocialKit",
+			targets: ["DiscordSocialKit"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/rryam/MusadoraKit", from: "6.1.0")
+	],
+	targets: [
+		.target(
+			name: "DiscordSocialKit",
+			dependencies: [
+				"MusadoraKit",
+				"DiscordSDK",
+			]),
+		.binaryTarget(
+			name: "DiscordSDK",
+			path: "Frameworks/discord_partner_sdk.xcframework"
+		),
+		.testTarget(
+			name: "DiscordSocialKitTests",
+			dependencies: ["DiscordSocialKit"]
+		),
+	]
 )
